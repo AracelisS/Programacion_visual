c2a0d186392cdf5b045b1906ed259f141dbf77b0
Aracelis Severino <89114480+AracelisS@users.noreply.github.com>
2022-01-19 19:45:30 +0000

    hh

--- modulo1/matematica/multi_signo.py
+++ modulo1/matematica/multi_signo.py
@@ -4,6 +4,6 @@ numero1 = int(input())
 print("Digite el segundo valor: ")
 numero2 = int(input())
 acu = 0
-for multi in range(1,numero2+1,1):
+for multi in range(numero2):
   acu = acu + numero1
 print("El resultado es: ", acu)
