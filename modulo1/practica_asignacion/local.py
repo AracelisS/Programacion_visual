bad574e15516ca6a8198c31ec707812f820920fc
Aracelis Severino <89114480+AracelisS@users.noreply.github.com>
2022-01-19 13:54:45 +0000

    Local

---
+++ modulo1/practica_asignacion/local.py
@@ -0,0 +1,9 @@
+print("Ganancia de local")
+print("Cuanto ganas por mes: ")
+ganasmes = int(input())
+print("Cuanto pagas de renta: ")
+renta = int(input())
+acu = 0
+acu= acu + ganasmes * 6
+print("Ganancia a 6 meses: ")
+print(acu)
