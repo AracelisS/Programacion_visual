72a2ed0bbbe7eca7af1e80b853c5fc431639a79c
Aracelis Severino <89114480+AracelisS@users.noreply.github.com>
2022-01-26 18:57:31 +0000

    el ocho del for

---
+++ modulo1/ciclo_for/ocho.py
@@ -0,0 +1,68 @@
+print("Tabla de multiplicar del 1 al 8")
+print("Tabla de multiplicar del 1")
+cont = 1
+for multi in range(1,13) :
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("1 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 2")
+cont = 2
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("2 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 3")
+cont = 3
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("3 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 4")
+cont = 4
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("4 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 5")
+cont = 5
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("5 por ",a, "=", corer)
+
+
+print("Tabla de multiplicar del 6")
+cont = 6
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("6 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 7")
+cont = 7
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("7 por ",a, "=", corer)
+
+print("Tabla de multiplicar del 8")
+cont = 8
+for multi in range(1,13):
+  if multi % 2 == 0:
+    a = multi
+    corer = cont*multi
+    print("8 por ",a, "=", corer)
+
+
+
