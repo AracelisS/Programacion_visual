5943c184e0e4090b044fbc8dd8092a51849c46ac
Aracelis Severino <89114480+AracelisS@users.noreply.github.com>
2022-02-08 18:25:58 +0000

    CURSO

---
+++ curso python/seguiente.py
@@ -0,0 +1,23 @@
+#def imprimir_mensaje():
+#  print("hola mundo")
+ # print("estoy aprendiendo a usar funciones")
+  #print("hola llego juanita")
+def conversacion(mensaje):
+  print("gracias por todo")
+  print("Te estaremos llamando")
+  print(mensaje)
+  print("Adios")
+opcion= input("Elige una opcion (1,2,3): ")
+if opcion== "1":
+ conversacion("Elegiste la opcion1")
+
+elif opcion=="2":
+  conversacion("Elegiste la opcion2")
+
+elif opcion=="3":
+  conversacion("Elegiste la opcion3")
+
+else: 
+  print("Escribe la opcion correcta ")
+  
+
