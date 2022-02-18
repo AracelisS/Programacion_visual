349daea6c82d7e40d9f0284c256a066aaa527e89
Aracelis Severino <89114480+AracelisS@users.noreply.github.com>
2022-02-18 18:54:13 +0000

    curso python

---
+++ curso python/merodos.py
@@ -0,0 +1,16 @@
+#metodos
+#upper()
+#capitalize()
+#strip()
+#lower()
+#replace()
+#[]
+#slices
+
+print("Digita tu nombre: ")
+nombre= input()
+nombre = nombre.capitalize() 
+
+print (nombre[0:3])
+
+
---
+++ curso python/palindromo.py
@@ -0,0 +1,22 @@
+def palindromo(palabra):
+  palabra = palabra.replace(' ','' )
+  palabra= palabra.lower()
+  palabra_invertida= palabra[::-1]
+  if palabra == palabra_invertida: 
+    return True
+  else: 
+    return False 
+
+
+def rum():
+  print("Escribe una palabra: ")
+  palabra = input()
+  es_palindromo =palindromo(palabra)
+  if es_palindromo = True: 
+    print("La palabra es palindromo")
+  else:
+    print("La palabra no es palindromo")
+
+
+if __name__ == '__main__':
+  rum()
